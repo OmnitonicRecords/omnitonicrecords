72d562d31a4191b545a34bf45e38dc8c2480a450
OmnitonicRecords <130651800+OmnitonicRecords@users.noreply.github.com>
2025-09-24 16:10:32 +0800

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',   // ← 加这一行，其余不动
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
